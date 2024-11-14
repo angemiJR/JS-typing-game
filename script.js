20de55fe583199b7c7d17640a0e134b8fa0e9531
angemiJR <111733781+angemiJR@users.noreply.github.com>
2024-11-14 12:16:28 +0100

    first part done

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// Variables for the DOM elements
-
-// Array
+// Array of words
 const words = [
   "dependent",
   "dog",
@@ -23,16 +21,43 @@ const words = [
   "north",
 ];
 
+
+let currentWord;
+let score = 0; 
+const typingWords = document.querySelector("h2"); // Element displaying the word
+const scoreElement = document.getElementById("score"); // Score display element
+const input = document.getElementById("text"); // Input field
+let timeLeft = 10;
+const timerElement = document.getElementById("time)"); // Timer display element
+
 function addWordToDOM() {
-  const randomWord = words[Math.floor(Math.random() * words.length)];
-  let typingWords = document.querySelector("h2");
-  typingWords.textContent = randomWord;
+  currentWord = words[Math.floor(Math.random() * words.length)]; 
+  typingWords.textContent = currentWord;
 }
-addWordToDOM(); 
 
 
 function updateScore() {
-  let score = document.getElementById("score");
-  score = 0;
 
+  input.addEventListener("input", function (e) {
+    const typedWords = e.target.value.trim(); 
+
+    // Check if typed word matches the current word
+    if (typedWords === currentWord) {
+      score++; 
+      scoreElement.textContent = score;
+      addWordToDOM(); 
+      input.value = ""; 
+
+    } else if (!currentWord.startsWith(typedWords)) {
+     
+      typingWords.textContent = "Game over!";
+      score = 0;
+      scoreElement.textContent = score; 
+      input.value = "";  
+    }
+  });
 }
+
+
+addWordToDOM();
+updateScore();
